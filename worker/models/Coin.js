cf61682c9aac8533c5c0278040515f88fbb29e89
Borxes <yurasherman+github@gmail.com>
2019-04-02 17:47:35 +0300

    moved worker to a separate repo

--- worker/models/Coin.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const { Schema } = mongoose;
-
-const coinSchema = new Schema({
-  name: String,
-});
-
-mongoose.model('coins', coinSchema);
--- worker/models/Config.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const { Schema } = mongoose;
-
-const configSchema = new Schema({
-  latestTweetID: String, // latest Tweet ID parsed
-});
-
-mongoose.model('config', configSchema);
